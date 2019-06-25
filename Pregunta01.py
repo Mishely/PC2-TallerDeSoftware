415fcaf0118e68a1816112dd69924cfa55b05747
Mishely Loyola <liceloyola.kmt@gmail.com>
2019-06-25 08:32:40 -0500

    preg

--- Pregunta01.py
+++ Pregunta01.py
@@ -1,4 +1,13 @@
+mtz1 = [[5],[4],],[[8],[2]]
 
+def EsPar ():
+    resultado = False
+    suma = 0
+    for i in range ():
+        suma = mtz1[i][i]
+        if suma % 2 == 0:
+            resultado = True 
+    return resultado
 
 
     
